c6de8e4910f4520509f9890dfca8301533aae2e6
ksss <co000ri@gmail.com>
2015-12-16 18:20:32 +0900

    More test

--- lib/freetype/error_test.rb
+++ lib/freetype/error_test.rb
@@ -2,12 +2,18 @@ require 'freetype/error'
 
 module FreeTypeErrorTest
   def test_all(t)
-    FreeType::Error::ERRORS.each do |code, (_klass, _code, _message)|
+    FreeType::Error::ERRORS.each do |code, (klass, _code, message)|
       err = FreeType::Error.find(code)
       unless FreeType::Error === err
         t.error('Error code miss generate')
       end
+      unless err.class == klass
+        t.error('Error.find return object miss')
+      end
       unless err.code == _code
+        t.error('Error.find return object miss')
+      end
+      unless err.message == message
         t.error('Error.find return object miss')
       end
     end
